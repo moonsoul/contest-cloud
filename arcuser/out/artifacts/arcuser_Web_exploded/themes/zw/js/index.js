3fb05669057c00fae84ddb558fed207f5715cd3f
moonsoul <707368171@qq.com>
2017-10-07 16:05:56 +0800

    spring cloud

--- arcuser/out/artifacts/arcuser_Web_exploded/themes/zw/js/index.js
+++
@@ -1,4 +0,0 @@
-$(function(){
-	$(".pagebox a:last").siblings().css("border-right","none")
-	$('.txtdl_4 dd a').tooltip({ track: true, delay: 0, showURL: false, extraClass: "right",top:15,left:-200 });
-});
